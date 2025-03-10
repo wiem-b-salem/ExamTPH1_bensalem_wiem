0053a2a05d358b844f3a7d28bc3d05029f4668f8
wiem <bensalemwi2m@gmail.com>
2025-03-10 09:18:38 +0100

    resolving conflict pt2

--- projet.py
+++ projet.py
@@ -6,7 +6,13 @@ def somme (T):
 		return s
 
 Data=[1,3,5]
-Som=sum(Data)
+if Data:
+	print('la somme est : ', sum(Data))
+	print('le min est : ', min(Data))
+	print('le max est : ', max(Data))
+else:
+	print('dossier vide')
 
 print('la somme est:" ,som)
+
 
